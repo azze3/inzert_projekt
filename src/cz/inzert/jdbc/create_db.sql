5906796ced5357b3efcff70e792d0e678d4e72af
gleva <glevicky.david@gmail.com>
2018-01-18 11:00:12 +0100

    zmena db

--- src/cz/inzert/jdbc/create_db.sql
+++ src/cz/inzert/jdbc/create_db.sql
@@ -4,85 +4,72 @@ USE `inzert`;
 SET foreign_key_checks = 0;
 
 DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `adress`;
 DROP TABLE IF EXISTS `advertisement`;
 DROP TABLE IF EXISTS `category`;
-DROP TABLE IF EXISTS `admin`;
 DROP TABLE IF EXISTS `rating`;
 DROP TABLE IF EXISTS `response`;
-
 SET foreign_key_checks = 1;
 
-CREATE TABLE `adress` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `zipCode` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `street` varchar(45) DEFAULT NULL, 
-  PRIMARY KEY (`id`)
-) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(45) DEFAULT NULL,
-  `lastName` varchar(45) DEFAULT NULL,
-  `login` varchar(45) DEFAULT NULL,
-  `phoneNumber` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `adress` int(11) DEFAULT NULL,
-  `rating` int(11) DEFAULT NULL,
-  `privileges` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT FK_AdressUser FOREIGN KEY (adress)
-  REFERENCES adress(id) 
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `phoneNumber` varchar(45),
+  `email` varchar(45) NOT NULL,
+  `rating` int(11) DEFAULT 0,
+  `privileges` int(2) DEFAULT 0, # 0 = user; 1 = admin
+  PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,  
+  `title` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `advertisement` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `description` varchar(1000) DEFAULT NULL,
-  `user` int(11) DEFAULT NULL,
-  `category` int(11) DEFAULT NULL,
-  `reported` int(5) DEFAULT NULL,
-  `created` date DEFAULT NULL,
+  `title` varchar(45) NOT NULL,
+  `description` varchar(1000) DEFAULT "",
+  `user` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  `reported` int(5) DEFAULT 0,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP,
   `price` float(10) DEFAULT 0,
-  # 0 = sell; 1 = buy
-  `type` tinyint default 0,
+  `location` varchar(10) DEFAULT "",
+  `type` tinyint default 0,  # 0 = sell; 1 = buy
   PRIMARY KEY (`id`),
   CONSTRAINT FK_UserAdvertisement FOREIGN KEY (user)
   REFERENCES user(id),
   CONSTRAINT FK_CategoryAdvertisement FOREIGN KEY (category)
   REFERENCES category(id)
- 
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `response` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `advertisement` int(11) DEFAULT NULL,
-  `user` int(11) DEFAULT NULL,
-  `text` varchar(45) DEFAULT NULL,  
+  `advertisement` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `text` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT FK_AdvertisementResponse FOREIGN KEY (advertisement)
-  REFERENCES response(id), 
+  REFERENCES response(id),
   CONSTRAINT FK_UserResponse FOREIGN KEY (user)
   REFERENCES `user`(id)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `rating` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userMaker` int(11) DEFAULT NULL,  
-  `userTaker` int(11) DEFAULT NULL,  
-  `value` float(2) DEFAULT NULL,    
+  `userMaker` int(11) NOT NULL,
+  `userTaker` int(11) NOT NULL,
+  `value` float(2) NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT FK_UserTakerRating FOREIGN KEY (userTaker)
-  REFERENCES user(id), 
+  REFERENCES user(id),
   CONSTRAINT FK_UserMakerRating FOREIGN KEY (userMaker)
-  REFERENCES user(id) 
+  REFERENCES user(id)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
